d621b1114f9d7662a691ff798ee9eb85616fab20
sepaev <sepaev@gmail.com>
2021-09-23 00:11:00 +0300

    API

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,54 @@
-import axios from "axios";
+import axios from 'axios';
 
-const apiUrl = "https://pixabay.com/api/";
-const key = "22594439-4316377fda5f0b6c1b052f095";
-const imageType = "photo";
-const orientation = "horizontal";
-const safesearch = "false";
-export const perPage = 12;
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '8948cf34f147d17edd39edcb74badce4';
 
-export const api = axios.create({
-  baseURL: apiUrl,
-  responseType: "json",
-  params: {
-    key,
-    image_type: imageType,
-    orientation,
-    safesearch,
-    per_page: perPage,
-  },
-});
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = { api_key: API_KEY, language: 'en-US' };
+
+////////////// TRANDING
+// https://api.themoviedb.org/3/trending/movie/{timeWindow}?api_key={api_key}
+// time_window - day/week
+export const fetchTrandingMovies = async (timeWindow = 'week') => {
+  return await axios.get(`/trending/movie/${timeWindow}`, {});
+};
+
+////////////// SEARCH
+// https://api.themoviedb.org/3/search/movie?api_key={api_key}&language=en-US&query={query}&page={page}&include_adult=false
+// query
+// page
+export const fetchMoviesByName = async (query, page = 1) => {
+  return await axios.get('/search/movie', {
+    params: {
+      query,
+      page,
+      include_adult: false,
+    },
+  });
+};
+
+////////////// FILM INFO
+// https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US
+// movie_id
+export const fetchMoviesById = async id => {
+  return await axios.get(`/movie/${id}`, {});
+};
+
+////////////// ACTORS
+// https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={api_key}&language=en-US
+// movie_id
+export const fetchActorsByMovieId = async id => {
+  return await axios.get(`/movie/${id}/credits`, {});
+};
+
+////////////// FILM REVIEWS
+// https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key={api_key}&language=en-US&page=1
+// movie_id
+// page
+export const fetchReviewsByMovieId = async (id, page = 1) => {
+  return await axios.get(`/movie/${id}/reviews`, {
+    params: {
+      page,
+    },
+  });
+};
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function HomeView() {
+  const [films, setFilms] = useState(null);
+
   return (
     <>
-      <p>Home page</p>
+      <h1>Trending today</h1>
+      {films &&
+        films.map(film => (
+          <li key={film.id}>
+            <Link to={`/movies/${film.id}`}>{film.title}</Link>
+          </li>
+        ))}
     </>
   );
 }
