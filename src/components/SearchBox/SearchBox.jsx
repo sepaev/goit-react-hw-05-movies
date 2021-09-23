59c9e74dad33e5b2f4d895da594f0169187a00a9
sepaev <sepaev@gmail.com>
2021-09-23 17:40:19 +0300

    searchBox

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,12 @@
+function SearchBox() {
+  return (
+    <div>
+      <label htmlFor=''>
+        <input type='text' />
+      </label>
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default SearchBox;
--- src/views/MoviesView.js
+++ src/views/MoviesView.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from 'react';
-import FilmInfo from '../components/FilmInfo/FilmInfo';
-import { fetchMoviesById } from '../services/api';
+import FilmInfo from '../components/FilmInfo';
+import SearchBox from '../components/SearchBox';
+import { fetchMoviesById, fetchMoviesByName } from '../services/api';
 
 function MovieView() {
   const [film, setFilm] = useState(null);
+  const [searchMode, setSearchMode] = useState(true);
+  const [query, setQuery] = useState('');
+  function onSearch(query) {
+    setQuery(query);
+  }
   const newFilmId = 604;
   useEffect(() => {
     async function fetchData() {
@@ -23,6 +29,27 @@ function MovieView() {
     }
     fetchData();
   }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetchMoviesByName(query);
+        if (response.status === 200) {
+          //   const { id, title, poster_path, popularity, overview, genres, release_date } = response.data;
+          console.dir(response.data);
+          //   setFilm({ id, title, poster_path, popularity, overview, genres, release_date });
+        } else {
+          throw new Error('Error - ' + response.status);
+        }
+      } catch (error) {
+        console.log('rejected   ' + error.message);
+        return null;
+      }
+    }
+    fetchData();
+  }, [query]);
+
+  if (searchMode) return <SearchBox onSearch={onSearch} />;
   return <div className='container'>{newFilmId && <FilmInfo film={film} />}</div>;
 }
 
